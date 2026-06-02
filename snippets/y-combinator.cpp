907780d06f79150d4871f508d7da84858c34018a
Dipankar <dipankar@share.xyz>
2026-06-02 13:30:22 +0530

    Make y-combinator snippet self-contained

--- snippets/y-combinator.cpp
+++ snippets/y-combinator.cpp
@@ -4,15 +4,18 @@
 //       for (int v : adj[u]) if (v != p) self(v, u);
 //   });
 //   dfs(root, -1);
+#include <utility>
+#include <type_traits>
+
 template <class F>
 struct y_combinator_result {
     F f;
     template <class... Args>
     decltype(auto) operator()(Args&&... args) {
-        return f(*this, forward<Args>(args)...);
+        return f(*this, std::forward<Args>(args)...);
     }
 };
 template <class F>
-y_combinator_result<decay_t<F>> y_combinator(F&& f) {
-    return {forward<F>(f)};
+y_combinator_result<std::decay_t<F>> y_combinator(F&& f) {
+    return {std::forward<F>(f)};
 }
