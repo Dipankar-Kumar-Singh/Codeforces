12674c21f4e394be205a5803eee53474c51909db
Dipankar <dipankar@share.xyz>
2026-06-08 16:40:30 +0530

    Refactor abc460d.cpp to improve neighbor retrieval and replace queue with Point struct; add c_cpp_properties.json for VSCode configuration

--- contest/atcoder/abc460/abc460d.cpp
+++ contest/atcoder/abc460/abc460d.cpp
@@ -5,10 +5,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int      long long
-#define all(x)   begin(x), end(x)
-#define nl       '\n'
-
 #ifdef CPH
     #include "/Users/dk/personal/Codeforces/debugging.h"
     template <class T> void _dbg(T x) { cerr << x << endl; }
@@ -24,7 +20,11 @@ constexpr array<pair<int, int>, 8> dirs{{
     { 1, -1}, { 1, 0}, { 1, 1}  // ↙ ↓ ↘
 }};
 
-
+struct Point{ 
+    int x; 
+    int y;
+    int generation;
+};
 
 void solve(int& tc) {
     int n , m ; 
@@ -32,37 +32,58 @@ void solve(int& tc) {
 
     vector<vector<bool>> vec(n, vector<bool>(m));
     vector<vector<int>> vis(n, vector<int>(m, -1));
-    queue<pair<int,int>> q ;
+    queue<Point> q ;
 
     function get_neighbours = [&](int i, int j) -> vector<pair<int,int>> {
         vector<pair<int,int>> neighbours = {}; 
         neighbours.reserve(8);
 
         for (const auto& [dx, dy] : dirs) {
+            if((i + dx) < 0 or (i + dx) >= n or (j + dy) >=m or (j + dy) < 0) continue;
             neighbours.emplace_back(i + dx, j + dy);
         }
         return neighbours ; 
     };
 
+    int black_count = 0 ; 
 
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m ; j++){
             char c ; cin >> c ; 
-            vec[i][j] = c == '#' ;
-            if(vec[i][j] == true) q.push({i,j});
+            vec[i][j] = (c == '#') ;
+            if(vec[i][j] == true) {
+                q.push(Point{i,j,0});
+                vis[i][j] = 0; 
+                black_count++ ; 
+            }
         }
     }
 
+    if(black_count == n * m){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                cout << "." ;
+            } cout << "\n";
+        }
+        return ;
+    }
 
     while(q.size()){
-        auto [x,y] = q.front();
+        auto [x,y, gen] = q.front();
         q.pop();
 
-        for (auto [nx, ny] : get_neighbours(x, y)) {
-            if(vis[nx][ny] != -1) continue ; 
-
-            if(vis[1] == )
+        for (auto& [nx, ny] : get_neighbours(x, y)) {
+            if (vis[nx][ny] != -1) continue;
+            vis[nx][ny] = gen + 1 ; 
+            q.push({nx, ny, vis[nx][ny]});
         }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(vis[i][j] == - 1 or vis[i][j] % 2) cout << "." ; 
+            else cout << "#" ;
+        } cout << "\n" ;
     }
 }
 
