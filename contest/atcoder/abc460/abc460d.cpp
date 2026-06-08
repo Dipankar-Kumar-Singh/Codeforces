d43b77f2196360cd57ac086083fcd6ddd766141b
Dipankar <dipankar@share.xyz>
2026-06-08 16:41:57 +0530

    Refactor code formatting and improve readability in abc460d.cpp

--- contest/atcoder/abc460/abc460d.cpp
+++ contest/atcoder/abc460/abc460d.cpp
@@ -6,46 +6,51 @@
 using namespace std;
 
 #ifdef CPH
-    #include "/Users/dk/personal/Codeforces/debugging.h"
-    template <class T> void _dbg(T x) { cerr << x << endl; }
-    template <class T, class... R> void _dbg(T x, R... r) { cerr << x << " | ", _dbg(r...); }
-    #define debug(...) cerr << "[" << #__VA_ARGS__ << "] = ", _dbg(__VA_ARGS__)
+#include "/Users/dk/personal/Codeforces/debugging.h"
+template <class T>
+void _dbg(T x) { cerr << x << endl; }
+template <class T, class... R>
+void _dbg(T x, R... r) { cerr << x << " | ", _dbg(r...); }
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "] = ", _dbg(__VA_ARGS__)
 #else
-    #define debug(...)
+#define debug(...)
 #endif
 
 constexpr array<pair<int, int>, 8> dirs{{
-    {-1, -1}, {-1, 0}, {-1, 1}, // ↖ ↑ ↗
-    { 0, -1},           { 0, 1}, // ←   →
-    { 1, -1}, { 1, 0}, { 1, 1}  // ↙ ↓ ↘
+    {-1, -1}, {-1, 0}, {-1, 1},  // ↖ ↑ ↗
+    {0, -1},
+    {0, 1},  // ←   →
+    {1, -1},
+    {1, 0},
+    {1, 1}  // ↙ ↓ ↘
 }};
 
-struct Point{ 
-    int x; 
+struct Point {
+    int x;
     int y;
     int generation;
 };
 
 void solve(int& tc) {
-    int n , m ; 
-    cin >> n >> m ;
+    int n, m;
+    cin >> n >> m;
 
     vector<vector<bool>> vec(n, vector<bool>(m));
     vector<vector<int>> vis(n, vector<int>(m, -1));
-    queue<Point> q ;
+    queue<Point> q;
 
-    function get_neighbours = [&](int i, int j) -> vector<pair<int,int>> {
-        vector<pair<int,int>> neighbours = {}; 
+    function get_neighbours = [&](int i, int j) -> vector<pair<int, int>> {
+        vector<pair<int, int>> neighbours = {};
         neighbours.reserve(8);
 
         for (const auto& [dx, dy] : dirs) {
-            if((i + dx) < 0 or (i + dx) >= n or (j + dy) >=m or (j + dy) < 0) continue;
+            if ((i + dx) < 0 or (i + dx) >= n or (j + dy) >= m or (j + dy) < 0) continue;
             neighbours.emplace_back(i + dx, j + dy);
         }
-        return neighbours ; 
+        return neighbours;
     };
 
-    int black_count = 0 ;
+    int black_count = 0;
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
@@ -60,31 +65,35 @@ void solve(int& tc) {
         }
     }
 
-    if(black_count == n * m){
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                cout << "." ;
-            } cout << "\n";
+    if (black_count == n * m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cout << ".";
+            }
+            cout << "\n";
         }
-        return ;
+        return;
     }
 
-    while(q.size()){
-        auto [x,y, gen] = q.front();
+    while (q.size()) {
+        auto [x, y, gen] = q.front();
         q.pop();
 
         for (auto& [nx, ny] : get_neighbours(x, y)) {
             if (vis[nx][ny] != -1) continue;
-            vis[nx][ny] = gen + 1 ; 
+            vis[nx][ny] = gen + 1;
             q.push({nx, ny, vis[nx][ny]});
         }
     }
 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            if(vis[i][j] == - 1 or vis[i][j] % 2) cout << "." ; 
-            else cout << "#" ;
-        } cout << "\n" ;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (vis[i][j] == -1 or vis[i][j] % 2)
+                cout << ".";
+            else
+                cout << "#";
+        }
+        cout << "\n";
     }
 }
 
